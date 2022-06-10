d491ab204a104ddc906d7ab6902dcbe51fb42171
David Roberts <david@crushing.digital>
2022-06-10 09:58:57 +0100

    Tailwind icons, vertically align 3 links at all times

--- src/components/DeveloperList.vue
+++ src/components/DeveloperList.vue
@@ -4,8 +4,21 @@
       <li v-for="dev in candidates">
         <div class="grid grid-cols-12 mb-4">
           <div class="flex flex-col justify-center invisible sm:visible">
-            <span class="material-symbols-outlined text-5xl">
-              account_circle
+            <span class="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
             </span>
           </div>
           <div class="col-span-8 flex flex-col justify-evenly">
@@ -14,33 +27,96 @@
                 <span class="text-sm sm:text-base"
                   >{{ dev.firstname }} {{ dev.lastname }}</span
                 >
-                <span
-                  v-if="dev.verified"
-                  class="material-symbols-outlined text-green-500 text-sm"
-                  >priority</span
-                >
-                <span
-                  v-if="dev.approved"
-                  class="material-symbols-outlined text-yellow-400 text-sm"
-                  >star</span
-                >
+                <span v-if="dev.verified" class="text-green-500 text-sm"
+                  ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                    /></svg
+                ></span>
+                <span v-if="dev.approved" class="text-yellow-400 text-sm"
+                  ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    /></svg
+                ></span>
               </div>
               <div
                 class="flex flex-row justify-start sm:justify-end py-2 sm:py-0"
               >
-                <span class="material-symbols-outlined text-slate-700">
-                  hourglass_bottom </span
-                ><span class="mr-2 text-xs text-slate-700"
+                <span class="text-slate-700">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                    <path
+                      d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                    />
+                  </svg> </span
+                ><span class="mr-2 text-sm text-slate-700"
                   >{{ dev.yoe }}yrs</span
                 >
-                <span class="material-symbols-outlined text-yellow-700">
-                  paid </span
-                ><span class="mr-2 text-xs text-yellow-700"
+                <span class="text-yellow-700">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg> </span
+                ><span class="mr-2 text-sm text-yellow-700"
                   >{{ dev.rate / 1000 }}k</span
                 >
-                <span class="material-symbols-outlined text-blue-600">
-                  public </span
-                ><span class="mr-2 text-xs text-blue-600">{{
+                <span class="text-blue-600">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    /></svg
+                ></span>
+                <span class="mr-2 text-sm text-blue-600">{{
                   dev.timezone
                 }}</span>
               </div>
@@ -59,16 +135,55 @@
             </div>
           </div>
           <div
-            class="col-span-3 border-0 flex flex-col sm:flex-row justify-center sm:justify-end align-middle"
+            class="col-span-3 border-0 flex flex-col justify-between align-middle"
           >
-            <div class="material-symbols-outlined text-5xl text-center">
-              crop_square
+            <div class="flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
             </div>
-            <div class="material-symbols-outlined text-5xl text-center">
-              crop_square
+            <div class="flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
             </div>
-            <div class="material-symbols-outlined text-5xl text-center">
-              crop_square
+            <div class="flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                />
+              </svg>
             </div>
           </div>
         </div>
