11fb7138159885e923cd1e0783fff0cc7a6cac82
David Roberts <david@crushing.digital>
2022-06-28 11:06:49 +0100

    added AUTH_REDIRECT_URL to handle dev environment

--- src/composables/useAuthUser.ts
+++ src/composables/useAuthUser.ts
@@ -8,12 +8,13 @@ const user = ref<User|null>(null)
 export default function useAuthUser() {
 
     const { supabase } = useSupabase();
-    
+    const redirectUrl = import.meta.env.AUTH_REDIRECT_URL
+
     /**
      * Login with google, github, etc
      */
     const login = async (provider: Provider) => {
-        const { user, error } = await supabase.auth.signIn({ provider });
+        const { user, error } = await supabase.auth.signIn({ provider }, redirectUrl ? { redirectTo: redirectUrl } : undefined);
         if (error) throw error;
     };
 
