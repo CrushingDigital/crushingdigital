14358598b4919b5506e3e1614c7d879b8dc4bf67
David Roberts <david@crushing.digital>
2022-06-28 11:24:35 +0100

    Prepend VITE_ to environment vars

--- src/composables/useAuthUser.ts
+++ src/composables/useAuthUser.ts
@@ -2,18 +2,20 @@ import { Provider } from "@supabase/supabase-js";
 import useSupabase from "./useSupabase";
 import { ref } from "vue";
 import { User } from "@supabase/supabase-js";
+import { exit } from "process";
 
 const user = ref<User|null>(null)
 
 export default function useAuthUser() {
 
     const { supabase } = useSupabase();
-    const redirectUrl = import.meta.env.AUTH_REDIRECT_URL
 
     /**
      * Login with google, github, etc
      */
     const login = async (provider: Provider) => {
+        const redirectUrl = import.meta.env.VITE_AUTH_REDIRECT_URL
+        console.log(redirectUrl)
         const { user, error } = await supabase.auth.signIn({ provider }, redirectUrl ? { redirectTo: redirectUrl } : undefined);
         if (error) throw error;
     };
