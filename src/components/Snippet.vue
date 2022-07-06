4012814d824b5e70ea62762d4d0098f579cc0e83
David Roberts <david@crushing.digital>
2022-07-06 09:44:50 +0100

    changed background colour and icon colours on snippet

--- src/components/Snippet.vue
+++ src/components/Snippet.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="!dev"><h3>Loading...</h3></div>
-  <div v-else class="flex justify-between p-4 bg-slate-50 rounded-2xl mb-1">
+  <div v-else class="flex justify-between p-4 bg-slate-100 rounded-2xl mb-1">
     <div class="flex flex-col justify-evenly">
       <div class="flex flex-col sm:flex-row justify-between">
         <div class="flex flex-row align-start">
@@ -79,7 +79,7 @@
             </svg> </span
           ><span class="mr-2 text-xs text-slate-700 sm:mt-1">{{ dev.yoe }}yrs</span>
           <!-- ********* RATE **************** -->
-          <span class="text-yellow-700 sm:mt-1">
+          <span class="text-slate-700 sm:mt-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-4 w-4"
@@ -94,9 +94,9 @@
                 d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg> </span
-          ><span class="mr-2 text-xs text-yellow-700 sm:mt-1">{{ dev.rate / 1000 }}k</span>
+          ><span class="mr-2 text-xs text-slate-700 sm:mt-1">{{ dev.rate / 1000 }}k</span>
           <!-- ********* TIMEZONE **************** -->
-          <span class="text-green-600 text-xxs sm:mt-1">
+          <span class="text-slate-700 text-xxs sm:mt-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-4 w-4"
@@ -111,7 +111,7 @@
                 d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               /></svg
           ></span>
-          <span class="text-xs text-green-600 sm:mt-1">{{ dev.timezone }}</span>
+          <span class="text-xs text-slate-700 sm:mt-1">{{ dev.timezone }}</span>
         </div>
       </div>
       <!-- ********* BLURB **************** -->
