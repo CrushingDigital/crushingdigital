93915291499ce73dda1db37570ec83dffc3c8da1
David Roberts <david@crushing.digital>
2022-07-21 15:45:53 +0100

    Added Java skill. New colors for exising skills

--- src/pages/Playground.vue
+++ src/pages/Playground.vue
@@ -3,6 +3,7 @@
     <i class="fa-solid fa-star text-[300px] my-20 text-yellow-400"></i>
     <i class="fa-solid fa-clipboard-check text-[300px] my-20 text-green-500"></i>
     <i class="fa-solid fa-fire-flame-curved text-[300px] my-20 text-secondary"></i>
+    <i class="fa-solid fa-bug text-[300px] my-20 text-red-700"></i>
   </div>
 </template>
 
