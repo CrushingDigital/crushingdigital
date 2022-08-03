2b1223cb8e0549782eae5c8c3918a42ca19a2e86
David Roberts <david@crushing.digital>
2022-08-03 13:32:52 +0100

    added getEventsForUser

--- src/composables/useEvent.ts
+++ src/composables/useEvent.ts
@@ -6,6 +6,7 @@ import { ref } from 'vue'
 const { user } = useAuthUser()
 const { supabase } = useSupabase()
 const events = ref<CDEvent[] | null>([])
+const personalEvents = ref<CDEvent[] | null>([])
 
 const addEvent = async (eventType: string, eventDescr: string, note: string | null, user_id: string) => {
   let { data, error } = await supabase.from('events').insert([
@@ -34,6 +35,24 @@ const getEvents = async (): Promise<CDEvent[]> => {
   return data as Array<CDEvent>
 }
 
+const getEventsForUser = async (user_id: string): Promise<CDEvent[]> => {
+  if (user_id == undefined) {
+    personalEvents.value = []
+    return personalEvents.value
+  }
+
+  let { data, error } = await supabase
+    .from('events')
+    .select('*')
+    .eq('user_id', user_id)
+    .order('created_at', { ascending: false })
+
+  if (error) throw error
+
+  personalEvents.value = data
+  return data as Array<CDEvent>
+}
+
 export default function useEvents() {
-  return { addEvent, getEvents, events }
+  return { addEvent, getEvents, getEventsForUser, events, personalEvents }
 }
