376cadbbefc35f62363d2d800fdd924be4d9f31a
David Roberts <david@crushing.digital>
2022-08-03 14:27:02 +0100

    Notifications to use Personal Events only

--- src/pages/Notifications.vue
+++ src/pages/Notifications.vue
@@ -5,17 +5,10 @@
 </template>
 
 <script setup lang="ts">
-  import useEvents from '@/composables/useEvent'
   import useAuthUser from '@/composables/useAuthUser'
-  import { onBeforeMount } from 'vue'
   import Events from '@/components/Events.vue'
 
   const { user } = useAuthUser()
-  const { events, getEvents } = useEvents()
-
-  onBeforeMount(async () => {
-    events.value = await getEvents()
-  })
 </script>
 
 <style scoped></style>
