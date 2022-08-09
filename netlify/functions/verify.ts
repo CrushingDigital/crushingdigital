9437e8eb0e27ed077f564d47d676af9b6627d57b
David Roberts <david@crushing.digital>
2022-08-09 15:58:19 +0100

    test sendgrid send

--- netlify/functions/verify.ts
+++ netlify/functions/verify.ts
@@ -1,6 +1,26 @@
 import { Handler } from '@netlify/functions'
+import sgMail from '@sendgrid/mail'
+
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY!
 
 const handler: Handler = async (event, context) => {
+  sgMail.setApiKey(SENDGRID_API_KEY)
+  const msg = {
+    to: 'davidproberts@gmail.com', // Change to your recipient
+    from: 'david@crushing.digital', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello World' }),
