91ea8739f94e2cd4fcff14ab82459fb3780d1356
David Roberts <david@crushing.digital>
2022-08-17 15:51:54 +0100

    center share features on mobile view

--- src/pages/Developers.vue
+++ src/pages/Developers.vue
@@ -1,20 +1,24 @@
 <template>
-  <div id="sharing" class="mt-4">
-    <span class="mr-2 threeLink">We could really use your help. Tell someone about us &raquo</span>
-    <ShareNetwork
-      class="mr-2"
-      v-for="social in socials"
-      :network="social.network"
-      :key="social.network"
-      :style="{ backgroundColor: social.color }"
-      :url="sharing.url"
-      :title="sharing.title"
-      :description="sharing.description"
-      :hashtags="sharing.hashtags"
-      :twitterUser="sharing.twitterUser"
+  <div id="sharing" class="sm:mt-4 flex flex-col sm:flex-row justify-start sm:justify-center items-center">
+    <span class="mr-2 threeLink text-center sm:text-left"
+      >We could really use your help. Tell someone about us &raquo</span
     >
-      <i :class="social.icon"></i>
-    </ShareNetwork>
+    <div class="flex flex-row sm:justify-start justify-center">
+      <ShareNetwork
+        class="mx-1"
+        v-for="social in socials"
+        :network="social.network"
+        :key="social.network"
+        :style="{ backgroundColor: social.color }"
+        :url="sharing.url"
+        :title="sharing.title"
+        :description="sharing.description"
+        :hashtags="sharing.hashtags"
+        :twitterUser="sharing.twitterUser"
+      >
+        <i :class="social.icon"></i>
+      </ShareNetwork>
+    </div>
   </div>
 
   <div class="collapse mt-4" v-if="candidates.length">
