4158e87f72867ece9954374bd94f5481fe2fa5ca
David Roberts <david@crushing.digital>
2022-08-23 15:06:54 +0100

    Refactored Developer list to extract the filters

---
+++ src/components/CandidateInlineFilters.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex justify-start flex-col sm:flex-row items-start sm:items-center my-2">
+    <div>
+      <span class="mr-2 flower">Filters:</span>
+      <label for="tz-modal" class="modal-button my-auto cursor-pointer mr-2">
+        <i class="fa-solid fa-earth-americas" title="Timezone"></i>
+      </label>
+      <label for="skills-modal" class="modal-button my-auto cursor-pointer mr-2">
+        <i class="fa-solid fa-tags" title="Skills"></i>
+      </label>
+      <label for="rate-modal" class="modal-button my-auto cursor-pointer mr-2">
+        <i class="fa-solid fa-sack-dollar" title="Rate"></i>
+      </label>
+      <label for="exp-modal" class="modal-button my-auto cursor-pointer mr-2">
+        <i class="fa-solid fa-graduation-cap" title="Experience"></i>
+      </label>
+      <label
+        title="Approved"
+        class="modal-button my-auto cursor-pointer mr-2"
+        @click="$emit('update:approved', !approved)"
+        :class="approved ? 'text-yellow-400' : 'text-gray-300'"
+      >
+        <i class="fa-solid fa-star"></i>
+      </label>
+      <label
+        title="Verified"
+        class="modal-button my-auto cursor-pointer mr-2"
+        @click="$emit('update:verified', !verified)"
+        :class="verified ? 'text-green-500' : 'text-gray-300'"
+      >
+        <i class="fa-solid fa-clipboard-check"></i>
+      </label>
+      <label
+        title="Review"
+        class="modal-button my-auto cursor-pointer mr-2"
+        @click="$emit('update:verify_req', !verify_req)"
+        :class="verify_req ? 'text-blue-400' : 'text-gray-300'"
+        v-if="memberships.includes('admin')"
+      >
+        <i class="fa-solid fa-eye"></i>
+      </label>
+      <label
+        title="Active"
+        class="modal-button my-auto cursor-pointer mr-2"
+        @click="$emit('update:active', !active)"
+        :class="active ? 'text-red-400' : 'text-gray-300'"
+        v-if="memberships.includes('admin')"
+      >
+        <i class="fa-solid fa-person-running"></i>
+      </label>
+    </div>
+
+    <input
+      type="text"
+      :value="searchText"
+      @input="$emit('update:searchText', ($event.target as HTMLInputElement).value)"
+      placeholder="Search by name..."
+      class="input input-bordered w-72 sm:ml-4"
+    />
+
+    <span class="flower ml-2">#{{ noDevs }} Developers</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps(['active', 'approved', 'verified', 'verify_req', 'noDevs', 'memberships', 'searchText'])
+  defineEmits(['update:searchText', 'update:active', 'update:verify_req', 'update:approved', 'update:verified'])
+</script>
+
+<style scoped></style>
