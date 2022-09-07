2e0d67fd15b383b433cc065fbaf18e3ddf25007e
David Roberts <david@crushing.digital>
2022-09-07 14:43:57 +0100

    Filtering jobs

--- src/pages/Jobs.vue
+++ src/pages/Jobs.vue
@@ -5,6 +5,20 @@
     <router-link to="/job/new" class="btn btn-accent"> Post a job for FREE!</router-link>
   </div>
   <div class="collapse mt-4" v-if="jobs.length">
+    <CandidateInlineFilters
+      :noDevs="filteredJobs.length"
+      :memberships="memberships"
+      v-model:searchText="searchVal"
+      v-model:approved="approved"
+      v-model:verified="verified"
+      v-model:verify_req="verify_req"
+      v-model:startTz="startTz"
+      v-model:endTz="endTz"
+      v-model:lowRate="lowRate"
+      v-model:highRate="highRate"
+      v-model:reqExp="reqExp"
+      v-model:active="active"
+    />
     <ul v-if="filteredJobs.length">
       <li v-for="job in filteredJobs">
         <JobSnippet :job="job" @skill-toggle="toggleSkill" />
@@ -17,10 +31,23 @@
   <div class="text-center mt-4" v-else>
     <h3>Nothing to see here!</h3>
   </div>
+
+  <CandidateModalFilters
+    v-model:skills="skills"
+    :filter-skills="filterSkills"
+    @skill-toggle="toggleSkill"
+    v-model:startTz="startTz"
+    v-model:endTz="endTz"
+    v-model:lowRate="lowRate"
+    v-model:highRate="highRate"
+    v-model:reqExp="reqExp"
+    v-model:searchVal="searchVal"
+  />
 </template>
 
 <script setup lang="ts">
   import { computed, onBeforeMount, ref } from 'vue'
+  import useAuthUser from '@/composables/useAuthUser'
   import useSkill from '@/composables/useSkill'
   import useJob from '@/composables/useJob'
   import { Job, Skill } from '@/types'
@@ -30,6 +57,7 @@
   import Share from '@/components/Share.vue'
 
   const { getJobs } = useJob()
+  const { user, memberships, getUserMemberships } = useAuthUser()
   const { getSkills, getJobSkillIds } = useSkill()
 
   const startTz = ref<number>(-12)
@@ -39,6 +67,7 @@
   const reqExp = ref<number>(0)
   const approved = ref<boolean>(false)
   const verified = ref<boolean>(false)
+  const verify_req = ref<boolean>(false)
   const searchVal = ref<string>('')
   const active = ref<boolean>(false)
   const filterSkills = ref<Skill[]>([])
@@ -46,6 +75,11 @@
   const jobs = ref<Array<Job>>([])
 
   onBeforeMount(async () => {
+    if (user.value && !memberships.value.length) {
+      let membershipCount = await getUserMemberships()
+      if (!membershipCount) throw new Error('Invalid membership credentials')
+    }
+
     let loadedJobs = await getJobs()
     if (loadedJobs instanceof Error) return false
 
@@ -65,6 +99,12 @@
       else if (approved.value && !job.approved) return false
       else if (verified.value && !job.verified) return false
       else if (active.value && !job.active) return false
+      else if (
+        searchVal.value &&
+        searchVal.value.length > 2 &&
+        !job.title.toLowerCase().includes(searchVal.value.toLowerCase())
+      )
+        return false
 
       const jobSkills = getJobSkillIds(job)
       const unmatched = filterSkills.value.filter((fskill) => !jobSkills.includes(fskill.id))
